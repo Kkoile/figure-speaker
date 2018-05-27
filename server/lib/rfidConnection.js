c258cbf1d34439bf1f67f6a799484edc61c7cf09
Nils Hirsekorn <Nils.Hirsekorn@sap.com>
2018-05-27 11:22:19 +0200

    better handling of environmental differences in rfid connection dependencies

--- server/lib/rfidConnection.js
+++ server/lib/rfidConnection.js
@@ -1,5 +1,22 @@
-if (process.env.USER === 'pi') {
-    module.exports = require('./rfidRC522Connection');
-} else {
-    module.exports = require('./rfidUSBConnection');
+var ApplicationError = require('./ApplicationError.js');
+var winston = require('winston');
+var aSupportedAdapters = ['./rfidRC522Connection', './rfidUSBConnection'];
+
+var oConnection = aSupportedAdapters.map(function (sAdapter) {
+    try {
+        oConnection = require(sAdapter);
+        winston.debug('Found an adapter for the following connection: ' + sAdapter);
+        return oConnection;
+    } catch (e) {
+        winston.debug('No NPM Module for the following adapter installed: ' + sAdapter);
+        return null;
+    }
+})
+    .find(function (oAdapter) {
+        return !!oAdapter;
+    });
+
+if (!oConnection) {
+    throw new ApplicationError('Could not establish an RFID connection, because no npm module is installed for any supported device.');
 }
+module.exports = oConnection;
