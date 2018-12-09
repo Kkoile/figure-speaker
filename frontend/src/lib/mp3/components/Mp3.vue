5c23831088fc0d9e3588d78af0d9cb7f00603cac
Nils Hirsekorn <Nils.Hirsekorn@sap.com>
2018-12-09 18:34:06 +0100

    [MP3 Upload] Made it possible to upload mp3 files fixes #3

--- frontend/src/lib/mp3/components/Mp3.vue
+++ frontend/src/lib/mp3/components/Mp3.vue
@@ -1,20 +1,69 @@
 <template xmlns:v-on="http://www.w3.org/1999/xhtml">
   <div>
-    <md-field>
-      <md-input v-model="$store.state.mp3.query" v-bind:placeholder="$t('mp3.url.placeholder')" v-on:keyup.enter="save"></md-input>
-      <md-button @click="save">{{ $t("common.save.button") }}</md-button>
-    </md-field>
+    <div class="md-title">{{ $t("mp3.list.title") }}</div>
+    <md-empty-state
+      v-if="$store.state.mp3.files.length === 0"
+      md-icon="play_circle_outline"
+    >
+      <div class="md-empty-state-label">{{ $t("mp3.noData.text") }}</div>
+      <md-button class="md-primary md-raised" v-on:click="openFileChoser()">{{ $t("mp3.uploadFirstFileNow.button") }}</md-button>
+      <div v-if="!!file" class="md-list-item-text">{{ $t("mp3.fileName.label") }} {{ file.name }}</div>
+      <md-button v-if="!!file" v-bind:disabled="!file" v-on:click="submitFile()">{{ $t("mp3.upload.button") }}</md-button>
+    </md-empty-state>
+    <md-list v-else>
+      <File v-for="file in $store.state.mp3.files"
+          v-bind:item="file"
+          v-bind:key="file" />
+    </md-list>
+    <input class="invisible" type="file" visible="false" id="file" ref="file" v-on:change="handleFileUpload()"/>
+    <div class="upload" v-if="$store.state.mp3.files.length > 0">
+      <md-button v-on:click="openFileChoser()">{{ $t("mp3.openFileChoser.button") }}</md-button>
+      <div class="md-list-item-text">{{ file.name }}</div>
+      <md-button v-bind:disabled="!file" v-on:click="submitFile()">{{ $t("mp3.upload.button") }}</md-button>
+    </div>
   </div>
 </template>
 
+<style scoped>
+  .upload {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+  }
+  .invisible {
+    display: none;
+  }
+</style>
+
 <script>
 
+import File from './File';
 export default {
   name: 'Mp3',
+  components: {
+    File
+  },
+  data () {
+    return {
+      file: ''
+    };
+  },
   methods: {
-    save: function () {
-      this.$store.dispatch('mp3/saveItem', this.$store.state.mp3.query);
+    submitFile () {
+      this.$store.dispatch('mp3/upload', this.file);
+    },
+
+    openFileChoser () {
+      this.$refs['file'].click();
+    },
+
+    handleFileUpload () {
+      this.file = this.$refs.file.files[0];
     }
+  },
+  beforeMount: function () {
+    this.$store.dispatch('mp3/loadAvailable');
   }
 };
 </script>
--- frontend/src/lib/mp3/store.js
+++ frontend/src/lib/mp3/store.js
@@ -1,16 +1,52 @@
+import axios from 'axios';
+
 const state = {
-  query: ''
+  query: '',
+  uploadStatus: null,
+  files: []
 };
 
 const mutations = {
   resetData (state) {
     state.query = '';
+    state.uploadStatus = null;
+    state.files = [];
+  },
+  updateUploadStatus (state, sStatus) {
+    state.uploadStatus = sStatus;
+  },
+  setAvailableMp3Files (state, aFiles) {
+    state.files = aFiles;
   }
 };
 
 const actions = {
   saveItem ({dispatch}, sUri) {
     dispatch('saveItem', sUri, { root: true });
+  },
+  loadAvailable ({commit}) {
+    axios.get('/data/mp3')
+      .then(function (oData) {
+        commit('setAvailableMp3Files', oData.data);
+      });
+  },
+  upload ({commit, dispatch}, oFile) {
+    var oFormData = new FormData();
+    oFormData.append('file', oFile);
+    axios.post('/data/mp3/upload',
+      oFormData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    ).then(function () {
+      commit('updateUploadStatus', 'SUCCESS');
+      dispatch('loadAvailable');
+    })
+      .catch(function () {
+        commit('updateUploadStatus', 'FAILURE');
+      });
   }
 };
 
